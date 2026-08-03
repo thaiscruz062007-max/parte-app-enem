3ee09c9a18e8a4cdc8390c307888608fc892c867
thaiscruz062007-max <thaiscruz062007@gmail.com>
2026-08-03 16:49:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Em que medida a centralização urbana e a infrestrutura docente escolar determinam as disparidades de desempenho no ENEM?")
-st. latex(r"y_i = \beta_0 + \beta_1 X_i +\epsilon_i")
+
